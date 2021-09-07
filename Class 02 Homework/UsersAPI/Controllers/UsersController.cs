56356334d8138ec4dfd46a3b32a8266964a04372
JanaSimjanovska <simjanovska.jana@gmail.com>
2021-09-07 02:59:58 +0200

    Class 02 Homework added

---
+++ Class 02 Homework/UsersAPI/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UsersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET: api/<UsersController>
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> LongNameForMethodThatGetsAllUsers()
+        {
+            return Ok(StaticDB.Users);
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public ActionResult<string> AnotherNonimportantMethodNameReturnsOneUser(int id)
+        {
+            try
+            {
+
+                if (id < 0)
+                {
+                    return BadRequest("Id cannot be a negative number.");
+                }
+                if (id >= StaticDB.Users.Count)
+                {
+                    return NotFound($"There is no user with id {id} in the database.");
+                }
+                return Ok(StaticDB.Users[id]);
+            }
+            catch
+            {
+                // Znam deka ne e dobra praksa da se vaka neuniformno napisani status kodovive, i pogore sakav da ja probam drugata sintaksa od dvete od cas, ama za 500 ne mozev da najdam ime za da go napisam kako pogornite sto se napisani.
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server Error");
+            }
+        }
+
+        [HttpGet("getThisRequestPath")]
+        public ActionResult<string> GetPath()
+        {
+            var request = Request;
+            return Ok(request.Path.ToString());
+        }
+    }
+}
---
+++ Class 02 Homework/UsersAPI/StaticDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersAPI
+{
+    public static class StaticDB
+    {
+        public static List<string> Users = new List<string>
+        {
+            "User123",
+            "JohnDoe",
+            "JaneDoe",
+            "Pepe"
+        };
+    }
+}
